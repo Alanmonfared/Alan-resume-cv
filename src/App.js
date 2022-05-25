21ebb81e6a1b3d9330ff4b37d9fa842fa3dd2342
Alanmonfared <74714682+Alanmonfared@users.noreply.github.com>
2022-05-25 20:22:29 +0200

    lord

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import Experience from "./components/experience/Experience";
+import Education from "./components/education/Education";
+import Internship  from "./components/internship/Internship";
+import Navbar from "./components/nav/Navbar";
+import About from "./components/about/About";
+import SocialMedia from "./global/SocialMedia";
+
+function App() {
+  return (
+    <>
+      <Router >
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/internship" element={<Internship  />} />
+          <Route path="/about" element={<About  />} />
+        </Routes>
+        <SocialMedia />
+      </Router>
+    </>
+  );
+}
+
+export default App;
